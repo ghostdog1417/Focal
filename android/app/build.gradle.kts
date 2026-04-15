a26de02f829e57415d575dd321f3c233698cac47
Srijan Dwivedi <66714676+ghostdog1417@users.noreply.github.com>
2026-04-16 02:59:57 +0530

    build fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 android {
-    namespace = "com.focal"
+    namespace = "com.focusnest"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.focal"
+        applicationId = "com.focusnest"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/study_buddy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/study_buddy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.focal
+package com.focusnest
 
 import io.flutter.embedding.android.FlutterActivity
 
